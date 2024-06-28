7e187cfa4285d61ad3744d7748a1f8b8a48511ef
Fernando TPR <it21835278@my.sliit.lk>
2024-06-28 19:39:56 +0530

    basic 1

--- NewProject/App.js
+++ NewProject/App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name,setName] = useState('pasan')
+  const [person,setPerson] = useState({
+    pname:'Jhone',
+    age:20
+  })
+
+  const clickHandler = () =>{
+    setName('Ranshika')
+    setPerson({
+      pname:'whick',
+      age:30
+    })
+  }
+
+  const reFresh=()=>{
+    setName('Pasan')
+    setPerson({
+      pname:'Jhone',
+    age:20
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>Pasan APP!</Text>
+      <Text>My name is {name}</Text>
+      <Text>2nd state {person.pname} and age {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='Update Status' onPress={clickHandler}/>
+        <Button title='Refresh' onPress={reFresh}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +44,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer:{
+    marginTop:20
+    
+  }
 });
